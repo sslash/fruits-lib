f8c5dafd57af530b15858cba5a51c092a2293f45
sslash <michaelgunnulfsen@gmail.com>
2016-01-19 10:05:35 +0100

    fix bug on auth errors

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -58,7 +58,7 @@ export default function reducer (state = initialState, action = {}) {
                 user: null,
                 token: null,
                 authError: action.error ?
-                    action.error.message : action.payload.errorMessage
+                    action.error.errorMessage : action.payload.errorMessage
             });
 
         case types.SIGNUP:
