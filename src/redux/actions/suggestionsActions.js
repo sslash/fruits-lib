59b35aa8d089e66b5be02eb98d2da6c8a551ee07
sslash <michaelgunnulfsen@gmail.com>
2016-02-20 15:30:17 +0100

    new datastructure for venues

--- src/redux/actions/suggestionsActions.js
+++ src/redux/actions/suggestionsActions.js
@@ -1,5 +1,5 @@
 import * as actions from '../constants/actionTypes';
-
+import {fromJS} from 'immutable';
 
 export function clear () {
     return { type: actions.SUGGESTIONS_CLEAR };
@@ -17,14 +17,24 @@ export function fetch (params) {
     };
 }
 
-export function fetchDetail (params) {
+export function fetchDetail (spot) {
+    const params = {
+        sourceId: spot.sourceId,
+        source: spot.source,
+        lat: spot.geometry.lat,
+        lng: spot.geometry.lng,
+        name: spot.name
+    };
+
+    const suggestion = fromJS(spot);
+
     return {
         types: [
             actions.SUGGESTIONS_DETAIL_FETCH,
             actions.SUGGESTIONS_DETAIL_FETCH_SUCCESS,
             actions.SUGGESTIONS_DETAIL_FETCH_FAIL
         ],
-        promise: ({req}) => req.get(`/venues/getSpicesByGEOandName`, { params }),
-        meta: params
+        promise: ({ req }) => req.get(`/venues/getSpicesByGEOandName`, { params }),
+        suggestion
     };
 }
