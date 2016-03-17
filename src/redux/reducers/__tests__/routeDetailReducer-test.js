29c5dea5a2f3186cab89d23b0a69309578b52b83
sslash <michaelgunnulfsen@gmail.com>
2016-03-17 14:06:14 +0100

    fetch and aggregate spices on server

--- src/redux/reducers/__tests__/routeDetailReducer-test.js
+++ src/redux/reducers/__tests__/routeDetailReducer-test.js
@@ -3,6 +3,8 @@ import reducer from '../routeDetailReducer';
 import * as types from '../../constants/actionTypes';
 import routeDirectionsResultFixture from './routeDirectionsResultFixture';
 import venueDirectionsResultFixture from './venueDirectionsResultFixture';
+import routeDetailFx from '../../../../test/fixtures/ROUTE_DETAIL_FETCH_SUCCESS_2';
+import spicesFx from '../../../../test/fixtures/ROUTE_DETAIL_VERTICE_SPICES_FETCH_SUCCESS';
 import multiplevenuesFixture from '../../../../test/fixtures/VENUES_DIRECTIONS_MATRIX_FETCH';
 import _isEqual from 'lodash/lang/isEqual';
 
@@ -113,5 +115,30 @@ describe('route detail reducer', () => {
 
         expect(afterState.getIn(['directionsMatrix', 'fetchingDirections'])).to.equal(true);
         expect(afterState.getIn(['directionsMatrix', 'fetchingDirectionsError'])).to.equal(false);
+    });
+
+    it('should handle ROUTE_DETAIL_VERTICE_SPICES_FETCH_SUCCESS', () => {
+        const beforeState = reducer(undefined, {
+            type: types.ROUTE_DETAIL_FETCH_SUCCESS,
+            payload: routeDetailFx
+        });
+
+        // pre condition
+        expect(beforeState.getIn(['route', 'id'])).to.equal(1785);
+        expect(beforeState.getIn(['route', 'vertices', '1', 'venue', 'id'])).to.equal(179);
+        expect(beforeState.getIn(['route', 'vertices', '0', 'venue', 'id'])).to.equal(905);
+        expect(beforeState.getIn(['route', 'vertices', '1', 'venue', 'venueSocial'])).to.be.null;
+        expect(beforeState.getIn(['route', 'vertices', '0', 'venue', 'venueSocial'])).to.be.null;
+
+        const afterState = reducer(beforeState, {
+            type: types.ROUTE_DETAIL_VERTICE_SPICES_FETCH_SUCCESS,
+            payload: spicesFx
+        });
+
+        expect(afterState.getIn(['route', 'vertices', '1', 'venue', 'venueSocial']).
+            foursquare_venue.meta.name).to.equal('Skimuseet');
+
+        expect(afterState.getIn(['route', 'vertices', '0', 'venue', 'venueSocial']).
+            instagram_venue.data[0].username).to.equal('roger_rolex');
     });
 });
---
+++ test/fixtures/ROUTE_DETAIL_VERTICE_SPICES_FETCH_SUCCESS.js
@@ -0,0 +1,334 @@
+const data = [
+    {
+        _id: "ede77ee0-b1fa-11e5-a806-9b08a400cb88",
+        createdAt: "2016-01-03T09:18:20.671Z",
+        updatedAt: "2016-01-26T12:42:33.434Z",
+        venueId: "179",
+        foursquare_venue: {
+            meta: {
+                name: "Skimuseet",
+                rating: 8.1,
+                id: "4b0588b5f964a52066d522e3"
+            },
+            photos: [
+                {
+                    prefix: "https://irs0.4sqi.net/img/general/",
+                    suffix: "/2027266_vi64NYdJomq7DZRsjOKp2A7zM5SHTuOoZt3kl9eOOKM.jpg",
+                    link: "https://foursquare.com/v/skimuseet/4b0588b5f964a52066d522e3",
+                    username: "Mani Brilliant"
+                },
+                {
+                    prefix: "https://irs0.4sqi.net/img/general/",
+                    suffix: "/1240777_VrhkRDThwGqboQ-Bvyr8KP5y33jLbGyNQT9OGK3tzxE.jpg",
+                    link: "https://foursquare.com/v/skimuseet/4b0588b5f964a52066d522e3",
+                    username: "Max Gross"
+                },
+                {
+                    prefix: "https://irs3.4sqi.net/img/general/",
+                    suffix: "/2027266_WpAcYEjWsaA1xpOw9SbajW6xNsDGRvdoocm07nTCeFE.jpg",
+                    link: "https://foursquare.com/v/skimuseet/4b0588b5f964a52066d522e3",
+                    username: "Mani Brilliant"
+                },
+                {
+                    prefix: "https://irs2.4sqi.net/img/general/",
+                    suffix: "/xzsePfhQKYHC5VGBmhh86v6o9uPWz-haB7xxnu79WlE.jpg",
+                    link: "https://foursquare.com/v/skimuseet/4b0588b5f964a52066d522e3",
+                    username: "Maria Alejandra Aveledo"
+                },
+                {
+                    prefix: "https://irs1.4sqi.net/img/general/",
+                    suffix: "/APVJPNOR2U5SMQMHQIMMPLY5ICWZ4ZIYRQQEZOC02TRUF31Q.jpg",
+                    link: "https://foursquare.com/v/skimuseet/4b0588b5f964a52066d522e3",
+                    username: "Jan Ove F"
+                },
+                {
+                    prefix: "https://irs1.4sqi.net/img/general/",
+                    suffix: "/23288579_kfnI8TDMJDISKpoWR2hOm2aNMkahG6IaXH3GGdVIGzY.jpg",
+                    link: "https://foursquare.com/v/skimuseet/4b0588b5f964a52066d522e3",
+                    username: "- Chris 18 - "
+                }
+            ],
+            tips: [
+                {
+                    username: "Anna Moberg",
+                    text: "Famous ski jump",
+                    photo: {
+                        prefix: "https://irs0.4sqi.net/img/user/",
+                        suffix: "/47838995-KKMNOKKCYOI3IKL4.jpg"
+                    },
+                    id: "5209557711d26d65bba9a927"
+                },
+                {
+                    username: "Dirk Gently",
+                    text: "You can go down with a zip-line (expensive) but it's exhilarating!",
+                    photo: {
+                        prefix: "https://irs2.4sqi.net/img/user/",
+                        suffix: "/FNXIZ41BU0TFAILJ.jpg"
+                    },
+                    id: "51ebe448498ebe476d71951b"
+                },
+                {
+                    username: "maitravels ",
+                    text: "During the summer months, do the zip lining! About 590NOK!",
+                    photo: {
+                        prefix: "https://irs0.4sqi.net/img/user/",
+                        suffix: "/14222584-PTQZQCDHVQDDJEHY.jpg"
+                    },
+                    id: "520a80ce11d2ee4d7a12649e"
+                },
+                {
+                    username: "Nilufer Strvk",
+                    text: "Norveçliler ayaklarında kayaklarıyla doğarlar derler ya işte kanıtı. Atlama rampasına çıkış, müze içerisindendir.",
+                    photo: {
+                        prefix: "https://irs2.4sqi.net/img/user/",
+                        suffix: "/9438521-G1MBPYWORTPYCNBN.jpg"
+                    },
+                    id: "53f906eb498ee29eee8c51e2"
+                },
+                {
+                    username: "Zuzana K",
+                    text: "Museum, Cafe shop, souvenir shop, ski simulator and view from ski jump tower in one! Awesome place",
+                    photo: {
+                        prefix: "https://irs1.4sqi.net/img/user/",
+                        suffix: "/IYNWONHZNXLJH5CH.jpg"
+                    },
+                    id: "52e2358111d29dd261ba2820"
+                }
+            ]
+        },
+        foursquare_venue_id: "4b0588b5f964a52066d522e3",
+        instagram_venue: {
+            data: [
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/s640x640/sh0.08/e35/12501505_1001812513245285_16325354_n.jpg"
+                        }
+                    },
+                    username: "camialisoon",
+                    link: "https://www.instagram.com/p/BA9Q09PDZPx/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/s640x640/sh0.08/e35/12599226_217313738611936_1137521434_n.jpg"
+                        }
+                    },
+                    username: "jessicaturci",
+                    link: "https://www.instagram.com/p/BA00SoCzVx-/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s640x640/sh0.08/e35/12407374_626869637452263_1947188415_n.jpg"
+                        }
+                    },
+                    username: "catsthanks",
+                    link: "https://www.instagram.com/p/BAx0HI5E4cm/"
+                }
+            ]
+        },
+        instagram_venue_id: "414252527",
+        yelp_venue: {
+            rating: 4,
+            snippet_text: "If you love skiing, this is a must-stop outside of central Oslo. And even if you don't, the view from the top of the ski jump is breathtaking, and the video...",
+            id: "skimuseet-i-holmenkollen-oslo"
+        },
+        __v: 1
+    },
+    { },
+    {
+        _id: "f486bf40-b1fa-11e5-a806-9b08a400cb88",
+        createdAt: "2016-01-03T09:18:32.672Z",
+        updatedAt: "2016-01-26T12:42:44.692Z",
+        venueId: "905",
+        foursquare_venue: {
+            meta: {
+                name: "Grefsenkollen",
+                rating: 8.6,
+                id: "4b818199f964a52000ab30e3"
+            },
+            photos: [
+                {
+                    prefix: "https://irs2.4sqi.net/img/general/",
+                    suffix: "/2715549_07lDOvWoIrKNrhibKwdffZbV63DuUohMG5zc10gLgDY.jpg",
+                    link: "https://foursquare.com/v/grefsenkollen/4b818199f964a52000ab30e3",
+                    username: "Hans-Petter Nygård-Hansen"
+                },
+                {
+                    prefix: "https://irs2.4sqi.net/img/general/",
+                    suffix: "/36717089_KaSD4RC1uBStOjjSOHSem9Y6mt8-KPYMcUG23AOAg4g.jpg",
+                    link: "https://foursquare.com/v/grefsenkollen/4b818199f964a52000ab30e3",
+                    username: "Gunnar Kopperud"
+                },
+                {
+                    prefix: "https://irs2.4sqi.net/img/general/",
+                    suffix: "/37586031_3BQWYasXjrgBOIx3KsaJCQT4sK9ACHVmHez8mQjbSCg.jpg",
+                    link: "https://foursquare.com/v/grefsenkollen/4b818199f964a52000ab30e3",
+                    username: "Hanne "
+                },
+                {
+                    prefix: "https://irs0.4sqi.net/img/general/",
+                    suffix: "/45891954_eOMnTQVFV2iZELDI9K4aXOUZUkL24b7q4Hfl2YYa4MM.jpg",
+                    link: "https://foursquare.com/v/grefsenkollen/4b818199f964a52000ab30e3",
+                    username: "Metin Yıldız"
+                },
+                {
+                    prefix: "https://irs2.4sqi.net/img/general/",
+                    suffix: "/636885_sVMF0B3nitgTbINX8PtezT8lbxp92MZHMqUfTXw-U6w.jpg",
+                    link: "https://foursquare.com/v/grefsenkollen/4b818199f964a52000ab30e3",
+                    username: "Sesilie Halland"
+                },
+                {
+                    prefix: "https://irs3.4sqi.net/img/general/",
+                    suffix: "/yij1EOmBi9GoXCpNjE0chuR7Ix-JFwBwNWFAJB_KGN0.jpg",
+                    link: "https://foursquare.com/v/grefsenkollen/4b818199f964a52000ab30e3",
+                    username: "Andreas Schjønhaug"
+                }
+            ],
+            tips: [
+                {
+                    username: "Oivind Kristiansen",
+                    text: "The best view and the best food in Oslo!",
+                    photo: {
+                        prefix: "https://irs1.4sqi.net/img/user/",
+                        suffix: "/F1KR1SBFADVNWPHL.jpg"
+                    },
+                    id: "4beed10170c603bb759c9bb4"
+                },
+                {
+                    username: "Kjetil Pettersen",
+                    text: "G for Grefsenkollen",
+                    photo: {
+                        prefix: "https://irs2.4sqi.net/img/user/",
+                        suffix: "/94029838-QVWZPFTDUYQDAO4S"
+                    },
+                    id: "540c643e498e68840951f455"
+                },
+                {
+                    username: "N Potet",
+                    text: "Sunny day so there is some mist over the Oslo, photo taken with a 200mm zoom",
+                    photo: {
+                        prefix: "https://irs0.4sqi.net/img/user/",
+                        suffix: "/blank_boy.png",
+                        default: true
+                    },
+                    id: "53c133bd498e027e85bdff19"
+                },
+                {
+                    username: "Gaelle Lo",
+                    text: "Majestic view over Oslo fjord",
+                    photo: {
+                        prefix: "https://irs0.4sqi.net/img/user/",
+                        suffix: "/55317071-CIHA4MAWFOCCFAVA.jpg"
+                    },
+                    id: "563049a1498e5bff4ef22c77"
+                },
+                {
+                    username: "Tord Sollie",
+                    text: "Great view.",
+                    photo: {
+                        prefix: "https://irs0.4sqi.net/img/user/",
+                        suffix: "/DDZEJ13QYZQY1AJV.jpg"
+                    },
+                    id: "54c01efd498ee3e2f38ccfe6"
+                }
+            ]
+        },
+        foursquare_venue_id: "4b818199f964a52000ab30e3",
+        instagram_venue: {
+            data: [
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e15/12353820_202228370111256_2043031271_n.jpg"
+                        }
+                    },
+                    username: "roger_rolex",
+                    link: "https://www.instagram.com/p/-9PLDplUYu/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e15/11850427_1464399273867072_590131367_n.jpg"
+                        }
+                    },
+                    username: "geirshb",
+                    link: "https://www.instagram.com/p/7Dw383uxnk/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xat1/t51.2885-15/e15/11356664_879378282097665_1846516774_n.jpg"
+                        }
+                    },
+                    username: "gylne_stunder",
+                    link: "https://www.instagram.com/p/6YONRPJd32/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xat1/t51.2885-15/e15/11373584_1015140868509758_736186696_n.jpg"
+                        }
+                    },
+                    username: "sindretolsen",
+                    link: "https://www.instagram.com/p/6Ng0nRRbux/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/e15/11330563_1587436704869989_1895656848_n.jpg"
+                        }
+                    },
+                    username: "ptibichik",
+                    link: "https://www.instagram.com/p/4MPduaiI6o/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e15/11349180_493774117446095_1774688748_n.jpg"
+                        }
+                    },
+                    username: "tabsen1",
+                    link: "https://www.instagram.com/p/3MrmuUk0AI/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e15/11311180_902477953157411_1182833684_n.jpg"
+                        }
+                    },
+                    username: "siljenitteberg",
+                    link: "https://www.instagram.com/p/2x2IaoGIET/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e15/11208008_1648431778719385_216043467_n.jpg"
+                        }
+                    },
+                    username: "riebekleens",
+                    link: "https://www.instagram.com/p/2OShewRsPx/"
+                },
+                {
+                    images: {
+                        standard_resolution: {
+                            url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e15/11116741_1606801176199313_631119271_n.jpg"
+                        }
+                    },
+                    username: "roger_rolex",
+                    link: "https://www.instagram.com/p/1GFOWWlUYS/"
+                }
+            ]
+        },
+        instagram_venue_id: "1098541",
+        yelp_venue: {
+            rating: 4.5,
+            snippet_text: "Great spot to hike and get one of the most beautiful view of Oslo fjord. It's closer to the city than going all the way up to Forgnerseteren. To get there...",
+            id: "grefsenkollen-oslo"
+        },
+        __v: 0
+    }
+]
+
+export default data;
