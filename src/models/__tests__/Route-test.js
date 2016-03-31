09881936e0e2a9d443fdde1996246dfd4b21193e
sslash <michaelgunnulfsen@gmail.com>
2016-03-31 06:59:07 +0200

    remove lodash bug

--- src/models/__tests__/Route-test.js
+++ src/models/__tests__/Route-test.js
@@ -14,6 +14,8 @@ describe('Routes', () => {
 
     it('Should get title', () => {
         const newRoute = Route.mapper(routeFixture);
+        console.log(`SAP! `, newRoute.getTimeAgo());
         expect(newRoute.getTitle()).to.equal(routeFixture.title);
+        expect(newRoute.getTimeAgo()).to.be.defined;
     });
 });
