43f96761ab8e7f5bec1bd7bb22aaefe1e529ed08
sslash <michaelgunnulfsen@gmail.com>
2016-04-06 19:19:49 +0200

    city tags

--- src/redux/actions/cityActions.js
+++ src/redux/actions/cityActions.js
@@ -11,6 +11,17 @@ export function fetch () {
     };
 }
 
+export function fetchCityBuckets (city) {
+    return {
+        types: [
+            actions.CITIES_BUCKETS_FETCH,
+            actions.CITIES_BUCKETS_FETCH_SUCCESS,
+            actions.CITIES_BUCKETS_FETCH_FAIL
+        ],
+        promise: ({ req }) => req.get(`/cities/${city}/buckets`)
+    };
+}
+
 export function selectCity (city) {
     return { type: actions.CITY_SELECT, city };
 }
