8271ae7bd00f432a8be63a80f5c75c542d5914ae
skeie <trulsske@iterate.no>
2016-09-04 11:04:06 +0200

    added people props to middleware (#40)

--- src/redux/middleware/AnalyticsMiddleware.js
+++ src/redux/middleware/AnalyticsMiddleware.js
@@ -1,21 +1,44 @@
 import * as types from '../constants/actionTypes';
-
 // those action types that are listed here will be tracked
 const whiteList = [];
 
+const peoplePropsWhiteList = [types.SIGNUP_SUCCESS, types.LOGIN_SUCCESS, types.FACEBOOK_LOGIN_SUCCESS];
+
 export default class AnalyticsMiddleware {
 
-    constructor (args = {}) {
-        this.tracking = args.tracking;
-
-        this.analyticsMiddleware = this.analyticsMiddleware.bind(this);
+    constructor(args = {}) {
+        if (args.tracking) {
+            if (args.tracking) {
+                this.tracking = args.tracking;
+                this.analyticsMiddleware = this.analyticsMiddleware.bind(this);
+            } else {
+                throw new Error('No tracking provided to analyticsMiddleware');
+            }
+        }
     }
 
-    analyticsMiddleware (store) {
+    analyticsMiddleware () {
         return next => action => {
+            this.addPeopleProps(action);
             (whiteList.indexOf(action.type) >= 0) &&
-            this.tracking.trackWithProperties(action.type, action.payload.user)
+                this.tracking.trackWithProperties(action.type, action.payload.user);
+
             return next(action);
+        };
+    }
+
+    addPeopleProps(action) {
+        if (peoplePropsWhiteList.indexOf(action.type) >= 0) {
+            if (this.tracking.identify) {
+                const { user: { mail } } = action.payload;
+                if (mail) {
+                    this.tracking.identify(mail);
+                    this.tracking.set('$email', mail);
+                } else {
+                    console.log('Error, no mail provided on user obj ', action.payload.user);
+                }
+            }
         }
+
     }
 }
