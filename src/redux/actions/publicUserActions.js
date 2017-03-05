6c262a1552b70b39768bb04ff8b23301da2eefe9
sslash <michaelgunnulfsen@gmail.com>
2017-03-05 09:57:04 +0100

    default params for fetch public profile

--- src/redux/actions/publicUserActions.js
+++ src/redux/actions/publicUserActions.js
@@ -1,6 +1,7 @@
 export function fetchPublicUserProfile (userId, params) {
+    let _params = params ? { params } : null;
     return {
         types: [types.PUBLIC_USER_FETCH, types.PUBLIC_USER_FETCH_SUCCESS, types.PUBLIC_USER_FETCH_FAIL],
-        promise: ({req}) => req.get(`/users/${userId}`, {params})
+        promise: ({req}) => req.get(`/users/${userId}`, _params)
     };
 }
